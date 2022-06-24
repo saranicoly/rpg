b8ddaccf4797a039c0d964c87037351f1dece0cf
Sara Lins <saranicoly0@gmail.com>
2022-06-24 18:20:20 -0300

    add initial conditions to professions

---
+++ profession.py
@@ -0,0 +1,19 @@
+class Profession:
+  def __init__(self, name, job):
+    self.name = name
+    self.job = self.professions[job]
+
+  professions = {
+    "warrior": {
+      "attributes": {"life": 20, "strength": 10, "agility": 5, "intelligence": 5},
+      "battle_modifiers": {"attack": 9, "speed": 4}
+    },
+    "thief": {
+      "attributes": {"life": 15, "strength": 4, "agility": 10, "intelligence": 4},
+      "battle_modifiers": {"attack": 12, "speed": 8}
+    },
+    "mage": {
+      "attributes": {"life": 12, "strength": 5, "agility": 6, "intelligence": 10},
+      "battle_modifiers": {"attack": 19, "speed": 4}
+    }
+  }
